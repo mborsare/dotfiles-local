74aea23d6d3d568dd0e686ca02c4383e9cb9d5b7
Mike Borsare <mborsare@gmail.com>
2018-12-15 16:08:49 -0500

    Vimrc: lightline config, add colorizer; Add crontab.local

---
+++ cron/daily-cleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
+brew update
+brew upgrade
+brew cleanup -s
+brew doctor
+brew missing
+
+apm upgrade -c false
+
+mas outdated
+mas upgrade
+
+npm update -g
+# echo “did you think to launch gem update “
+# echo “and pip ? pip freeze — local | grep -v ‘^\-e’ | cut -d = -f 1 | xargs pip install -U “
+
+asdf update
+asdf plugin-update --all
+
+# delete screenshots older than a week
+echo “Deleting old screenshots...“
+find $HOME/Screenshot* -mtime +6 -type f -delete
