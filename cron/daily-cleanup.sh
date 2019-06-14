440489a06402bbb46cb83603619f241983ca8e01
Mike Borsare <mborsare@gmail.com>
2019-06-14 10:24:35 -0400

    Cron: add daily cleanup for desktop files

--- cron/daily-cleanup.sh
+++ cron/daily-cleanup.sh
@@ -11,7 +11,7 @@ brew missing
 apm upgrade -c false
 
 # mas broke with macOS 10.13+
-  # mas outdated 
+  # mas outdated
   # mas upgrade
 
 npm update -g
@@ -24,3 +24,21 @@ asdf plugin-update --all
 # delete screenshots older than a week
 echo “Deleting old screenshots...“
 find $HOME/Screenshot* -mtime +6 -type f -delete
+
+# Clean desktop of files not modified in the last 24 hrs
+# and put the in a folder named archive
+# https://www.sitebase.be/automate-desktop-cleanup
+ARCHIVE_DIR=~/Desktop/archive
+DATE=$(date +"%Y%m%d")
+EXPIRE=1449 # in minutes (1449 = 1 day)
+
+cd ~/Desktop
+FOLDER="$ARCHIVE_DIR/$DATE"
+mkdir -p $FOLDER
+
+find . -type f -mmin +$EXPIRE -maxdepth 1 -exec ls -u {} \; | while read line; do
+  echo $line
+  mv "$line" "$FOLDER"
+done
+
+echo "Archived all expired files
